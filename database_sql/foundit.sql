68dc5b4c86b40d8788ac5d1e004d89995f63ad78
KenLanggook <98384623+KenLanggook@users.noreply.github.com>
2025-10-08 11:37:25 +0800

    Add files via upload

---
+++ database_sql/foundit.sql
@@ -0,0 +1,183 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 08, 2025 at 05:00 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `foundit`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `claimed_items`
+--
+
+CREATE TABLE `claimed_items` (
+  `id` int(11) NOT NULL,
+  `claimer_id` int(11) DEFAULT NULL,
+  `item_id` int(11) DEFAULT NULL,
+  `found_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `claimer`
+--
+
+CREATE TABLE `claimer` (
+  `claimer_id` int(11) NOT NULL,
+  `firstname` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `finder`
+--
+
+CREATE TABLE `finder` (
+  `finder_id` int(11) NOT NULL,
+  `firstname` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) DEFAULT NULL,
+  `department` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `found`
+--
+
+CREATE TABLE `found` (
+  `found_id` int(11) NOT NULL,
+  `item_id` int(11) DEFAULT NULL,
+  `room_no` int(11) DEFAULT NULL,
+  `finder_id` int(11) DEFAULT NULL,
+  `found_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item`
+--
+
+CREATE TABLE `item` (
+  `item_id` int(11) NOT NULL,
+  `item_name` varchar(100) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `claimed_items`
+--
+ALTER TABLE `claimed_items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `claimer_id` (`claimer_id`),
+  ADD KEY `item_id` (`item_id`),
+  ADD KEY `found_id` (`found_id`);
+
+--
+-- Indexes for table `claimer`
+--
+ALTER TABLE `claimer`
+  ADD PRIMARY KEY (`claimer_id`);
+
+--
+-- Indexes for table `finder`
+--
+ALTER TABLE `finder`
+  ADD PRIMARY KEY (`finder_id`);
+
+--
+-- Indexes for table `found`
+--
+ALTER TABLE `found`
+  ADD PRIMARY KEY (`found_id`),
+  ADD KEY `item_id` (`item_id`),
+  ADD KEY `finder_id` (`finder_id`);
+
+--
+-- Indexes for table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`item_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `claimed_items`
+--
+ALTER TABLE `claimed_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `claimer`
+--
+ALTER TABLE `claimer`
+  MODIFY `claimer_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `finder`
+--
+ALTER TABLE `finder`
+  MODIFY `finder_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `found`
+--
+ALTER TABLE `found`
+  MODIFY `found_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `item`
+--
+ALTER TABLE `item`
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `claimed_items`
+--
+ALTER TABLE `claimed_items`
+  ADD CONSTRAINT `claimed_items_ibfk_1` FOREIGN KEY (`claimer_id`) REFERENCES `claimer` (`claimer_id`),
+  ADD CONSTRAINT `claimed_items_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`),
+  ADD CONSTRAINT `claimed_items_ibfk_3` FOREIGN KEY (`found_id`) REFERENCES `found` (`found_id`);
+
+--
+-- Constraints for table `found`
+--
+ALTER TABLE `found`
+  ADD CONSTRAINT `found_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`),
+  ADD CONSTRAINT `found_ibfk_2` FOREIGN KEY (`finder_id`) REFERENCES `finder` (`finder_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
